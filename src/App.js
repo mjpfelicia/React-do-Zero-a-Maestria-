f19daecd0340236b52b5441e13736cbcf7652650
felicia <you@example.com>
2024-07-24 18:13:51 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Desafio</h1>
+      <h3>Instruções da tarefa:</h3>
+      <div className='descrição'>
+        <span>Concluindo a tarefa, adicionar elementos e mudar de cor usando CSS.</span>
+        <p>Entre no nosso arquivo App.js e adicione mais algum elemento HTML da.Sua escolha, crie uma regra de estilos em Aplicativo.css que altere a cor do seu elemento.Vá até o arquivo index.html e altere o título da aplicação na meta tag;</p>
+      </div>
+
+    </div>
+  );
+}
+
+
+
+export default App
+
+
