294a231fe472e5e8486b9abbd1d52716896e7dd9
felicia <you@example.com>
2024-08-14 15:17:26 -0300

    gerenciamento de dados de intult

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./MyForm.css"
 
 const MyForm = () => {
+    // 3 - gerenciamento de dados 
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+    console.log(name)
+
     return (
         <div className='box_form'>
-
+            {/* 1 - Criando form*/}
             <form>
                 <h1>Form</h1>
                 <div>
                     <label htmlFor='name'>Nome</label>
-                    <input type='text' name='name' placeholder='Digite seu nome'></input>
+                    <input type='text' name='name' placeholder='Digite seu nome' onChange={handleName}></input>
                 </div>
+
+                {/* 2 - Label envolvendo input */}
+                <label>
+                    <span>E-mail</span>
+                    <input type='email' name='email' placeholder='Digite seu e-mail'></input>
+                </label>
                 <input type='submit' value="Enviar"></input>
             </form>
         </div>
     )
 }
 
-export default MyForm
+export default MyForm;
